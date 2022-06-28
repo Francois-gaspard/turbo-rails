6827b61fb73888aaaf97ba3bfb9c5cc9d2f170f2
Francois G <fg@verumex.com>
2022-06-28 14:23:49 +0700

    Basic Properties CRUD

--- README.md
+++ README.md
@@ -5,8 +5,13 @@ This barebones app is the starting point of a small introduction to Turbo-Rails
 Run the following to set it up:
 ```
 bin/setup
+yarn install
 ```
 
+Seed your database:
+```
+bin/rails db:seed
+```
 And the following to start the server:
 ```
 bin/dev
---
+++ app/controllers/properties_controller.rb
@@ -0,0 +1,50 @@
+class PropertiesController < ApplicationController
+  before_action :set_property, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @properties = Property.all
+  end
+
+  def show
+  end
+
+  def new
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.new(property_params)
+
+    if @property.save
+      redirect_to properties_path, notice: "Property was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @property.update(property_params)
+      redirect_to properties_path, notice: "Property was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @property.destroy
+    redirect_to properties_path, notice: "Property was successfully destroyed."
+  end
+
+  private
+
+  def set_property
+    @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(:name)
+  end
+end
